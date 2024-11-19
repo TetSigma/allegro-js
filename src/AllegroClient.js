87b7232a0e6baf8e59f3d5530a999131fbedd604
Nikita Bondarenko <mangmagic2@gmail.com>
2024-11-19 14:27:49 +0100

    architecture setup + allegroclient code

---
+++ src/AllegroClient.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+
+class AllegroClient{
+    constructor(clientId, clientSecret){
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.authUrl = 'https://allegro.pl/auth/oauth/authorize'
+        this.tokenUrl = 'https://allegro.pl/auth/oauth/token'
+        this.token = null;
+        this.tokenExpiry = null;
+        this.httpClient = axios.create({
+            baseURL: this.authUrl,
+            headers:{
+                Accept: 'application/vnd.allegro.public.v1+json',
+            }
+        })
+    }
+    
+    async authenticate(){
+        if (this.token && new Date() < this.tokenExpiry){
+            return this.token
+        }
+
+        const credentials = Buffer.from(`${this.clientId}:${this.clientSecret}`).toString('base64');
+        const response = await axios.post(this.authUrl, null, {
+            params: {'grant type': 'authorization_code'},
+            headers:{
+                Authorization: `Basic ${credentials}`
+            }
+        })
+
+        this.token = response.data.access_token;
+        this.tokenExpiry = new Date(Date.now() + response.data.expires_in * 1000);
+        this.httpClient.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+        
+        return this.token;
+    }
+    
+    async request(method, endpoint, data = null, params = null){
+        await this.authenticate();
+        try{
+            const response = await this.httpClient.request({
+                method,
+                url: endpoint,
+                data,
+                params
+            });
+            return response.data
+        }catch(error){
+            console.error('API Request Failed:', error.response?.data || error.message);
+            throw error;
+        }
+    }
+
+}
+
+
+module.exports = AllegroClient;
